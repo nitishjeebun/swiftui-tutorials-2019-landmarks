7c17ae38287bb27a13f2a55f931b64597f25e8fe
Nitish Jeebun <nitish.jeebun@gmail.com>
2020-01-01 18:18:04 +0400

    02 - Create the List of Landmarks

---
+++ Landmarks/LandmarkList.swift
@@ -0,0 +1,24 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Nitish Jeebun on 01/01/2020.
+//  Copyright © 2020 Nitish Jeebun. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarkData[0])
+            LandmarkRow(landmark: landmarkData[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
