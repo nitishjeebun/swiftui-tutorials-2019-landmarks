b3571e6ff9aba90e454d52fb41c82c1a00b3ec08
Nitish Jeebun <nitish.jeebun@gmail.com>
2020-01-01 20:28:44 +0400

    03 - Use an Observable Object for Storage

---
+++ Landmarks/Models/UserData.swift
@@ -0,0 +1,15 @@
+//
+//  UserData.swift
+//  Landmarks
+//
+//  Created by Nitish Jeebun on 01/01/2020.
+//  Copyright © 2020 Nitish Jeebun. All rights reserved.
+//
+
+import SwiftUI
+import Combine
+
+final class UserData: ObservableObject  {
+    @Published var showFavoritesOnly = false
+    @Published var landmarks = landmarkData
+}
