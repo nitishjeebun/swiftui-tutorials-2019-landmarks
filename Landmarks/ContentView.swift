a9c168316631662d378a688b3a590db26cc4d972
Nitish Jeebun <nitish.jeebun@gmail.com>
2020-01-01 17:15:22 +0400

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Nitish Jeebun on 01/01/2020.
+//  Copyright © 2020 Nitish Jeebun. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
