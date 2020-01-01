f04f7c6299adf01d4006f40f564c5b2e7f70c281
Nitish Jeebun <nitish.jeebun@gmail.com>
2020-01-01 19:03:26 +0400

    03 - Mark the User’s Favorite Landmarks

--- Landmarks/LandmarkRow.swift
+++ Landmarks/LandmarkRow.swift
@@ -11,13 +11,19 @@ import SwiftUI
 struct LandmarkRow: View {
     var landmark: Landmark
     var body: some View {
-         HStack {
+        HStack {
             landmark.image
             .resizable()
             .frame(width: 50, height: 50)
                    Text(landmark.name)
             Spacer()
-               }
+
+            if landmark.isFavorite {
+                Image(systemName: "star.fill")
+                    .imageScale(.medium)
+                    .foregroundColor(.yellow)
+            }
+        }
     }
 }
 
