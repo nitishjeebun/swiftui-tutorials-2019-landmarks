825a3b89d97feec57b68afe1f8058048b9c4f0f0
Nitish Jeebun <nitish.jeebun@gmail.com>
2020-01-01 20:49:10 +0400

    04 - Create a Badge View

---
+++ Landmarks/Supporting Views/Badge.swift
@@ -0,0 +1,21 @@
+//
+//  Badge.swift
+//  Landmarks
+//
+//  Created by Nitish Jeebun on 01/01/2020.
+//  Copyright © 2020 Nitish Jeebun. All rights reserved.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    var body: some View {
+        Text("Badge")
+    }
+}
+
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
