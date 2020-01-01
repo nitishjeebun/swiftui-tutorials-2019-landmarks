e37046c70fdaca1694597a025a9f315ccdee5be9
Nitish Jeebun <nitish.jeebun@gmail.com>
2020-01-01 21:08:17 +0400

    04 - Combine the Badge Foreground and Background

---
+++ Landmarks/Supporting Views/RotatedBadgeSymbol.swift
@@ -0,0 +1,25 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Nitish Jeebun on 01/01/2020.
+//  Copyright © 2020 Nitish Jeebun. All rights reserved.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: .init(degrees: 5))
+    }
+}
