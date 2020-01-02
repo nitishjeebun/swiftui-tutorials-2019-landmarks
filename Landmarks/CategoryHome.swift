a9eb2a2bb54fb9c0fdcc3b4fd6b09de0c7e17218
Nitish Jeebun <nitish.jeebun@gmail.com>
2020-01-02 06:39:41 +0400

    06 - Add a Home View

---
+++ Landmarks/CategoryHome.swift
@@ -0,0 +1,25 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Nitish Jeebun on 02/01/2020.
+//  Copyright © 2020 Nitish Jeebun. All rights reserved.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView {
+            Text("Landmarks Content")
+            
+            .navigationBarTitle(Text("Featured"))
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
