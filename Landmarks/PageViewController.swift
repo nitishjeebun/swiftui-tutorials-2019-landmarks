ec202bb010b3583991ded6bc8c5c8f460fb325ec
Nitish Jeebun <nitish.jeebun@gmail.com>
2020-01-02 08:15:39 +0400

    08 - Track the Page in a SwiftUI View’s State

--- Landmarks/PageViewController.swift
+++ Landmarks/PageViewController.swift
@@ -11,7 +11,8 @@ import UIKit
 
 struct PageViewController: UIViewControllerRepresentable {
     var controllers: [UIViewController]
-
+    @Binding var currentPage: Int
+    
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
     }
@@ -22,6 +23,7 @@ struct PageViewController: UIViewControllerRepresentable {
             navigationOrientation: .horizontal)
         
         pageViewController.dataSource = context.coordinator
+        pageViewController.delegate = context.coordinator
         
         return pageViewController
     }
@@ -29,10 +31,11 @@ struct PageViewController: UIViewControllerRepresentable {
     
     func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
         pageViewController.setViewControllers(
-            [controllers[0]], direction: .forward, animated: true)
+            [controllers[currentPage]], direction: .forward, animated: true)
     }
     
-    class Coordinator: NSObject, UIPageViewControllerDataSource {
+    
+    class Coordinator: NSObject, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
         var parent: PageViewController
 
         init(_ pageViewController: PageViewController) {
@@ -63,6 +66,15 @@ struct PageViewController: UIViewControllerRepresentable {
                 return parent.controllers.first
             }
             return parent.controllers[index + 1]
+        }
+        
+        func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+            if completed,
+                let visibleViewController = pageViewController.viewControllers?.first,
+                let index = parent.controllers.firstIndex(of: visibleViewController)
+            {
+                parent.currentPage = index
+            }
         }
     }
 }
